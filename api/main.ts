2a162eb1ab9e8aff2d982b8b37289944afd748d0
s25429 <s25429@pjwstk.edu.pl>
2024-10-18 15:24:38 +0200

    added router and working api path, index.html does not work

--- api/main.ts
+++ api/main.ts
@@ -1,14 +1,13 @@
 import express from 'express'
+import apiRouter from './api'
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
 app.use(express.json())
 
-app.get('/api/test', (req, res) => {
-    res.json({ message: 'test' })
-})
+app.use('/api', apiRouter)
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
